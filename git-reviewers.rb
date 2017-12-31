f543ee729a8b739e907473749bf1271b36d3c81e
Albert Wang <git@albertyw.com>
2018-01-01 00:14:37 +0800

    Use builtin system.rm

--- git-reviewers.rb
+++ git-reviewers.rb
@@ -14,7 +14,7 @@ class GitReviewers < Formula
 
   def post_install
     system "#{prefix}/install.sh", prefix
-    system "rm", "#{prefix}/install.sh"
+    system.rm "#{prefix}/install.sh"
   end
 
   test do
