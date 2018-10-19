f2e3c49775937c8ba92b9325314d8d6b4cf24caf
Albert Wang <git@albertyw.com>
2018-10-18 21:52:42 -0700

    Update git-browse v2.7.0

--- git-browse.rb
+++ git-browse.rb
@@ -1,8 +1,8 @@
 class GitBrowse < Formula
   desc "Open git files, directories, commits, and branches in the browser"
   homepage "https://github.com/albertyw/git-browse"
-  url "https://github.com/albertyw/git-browse/archive/v2.6.0.tar.gz"
-  sha256 "73d1fee5a62c331c860551da99fc4d02e69423e2aa7d895e64580b13dbe4a27d"
+  url "https://github.com/albertyw/git-browse/archive/v2.7.0.tar.gz"
+  sha256 "99387b8cd704e2017c9fffce45a9c40522ac789b956ad659bd79ea5b793fbd78"
 
   head "https://github.com/albertyw/git-browse.git", :using => :git
 
