26fb88bbb88d5d7cc0e146c7e3b2843d1ab7073c
Albert Wang <git@albertyw.com>
2019-02-23 00:16:52 -0800

    Update git-reviewers to v0.12.2

--- git-reviewers.rb
+++ git-reviewers.rb
@@ -1,8 +1,8 @@
 class GitReviewers < Formula
   desc "Open git files, directories, commits, and branches in the reviewersr"
   homepage "https://github.com/albertyw/git-reviewers"
-  url "https://github.com/albertyw/git-reviewers/archive/v0.12.1.tar.gz"
-  sha256 "326cb99e4a1a890d4f0ad773241f7acd5924fe038688b6870a8157380bb0c6bb"
+  url "https://github.com/albertyw/git-reviewers/archive/v0.12.2.tar.gz"
+  sha256 "4ba8d58160103f6de6304eb39bda43246c9dd212dbc47bed252ed62dae4959dd"
 
   head "https://github.com/albertyw/git-reviewers.git", :using => :git
 
