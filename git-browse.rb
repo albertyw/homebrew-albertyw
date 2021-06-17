379af65e2c10a3e5b4acb8457e8c0e66b46d224a
Albert Wang <git@albertyw.com>
2021-06-16 23:44:40 -0700

    Fix rubocop issues

--- git-browse.rb
+++ git-browse.rb
@@ -1,10 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
+# GitBrowse is a homebrew formula for the git-browse cli tool
 class GitBrowse < Formula
   desc "Open git files, directories, commits, and branches in the browser"
   homepage "https://github.com/albertyw/git-browse"
   url "https://github.com/albertyw/git-browse/archive/v2.9.2.tar.gz"
   sha256 "124e573a6105ad2c3067bee9afa3430c39e01dc55023e9ffb632d687f1b0564a"
 
-  head "https://github.com/albertyw/git-browse.git", :using => :git
+  head "https://github.com/albertyw/git-browse.git", using: :git
 
   depends_on "python"
 
--- git-reviewers.rb
+++ git-reviewers.rb
@@ -1,10 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
+# GitReviewers is a homebrew formula for the git-reviewers cli tool
 class GitReviewers < Formula
   desc "Open git files, directories, commits, and branches in the reviewersr"
   homepage "https://github.com/albertyw/git-reviewers"
   url "https://github.com/albertyw/git-reviewers/archive/v0.13.2.tar.gz"
   sha256 "c70fed9333df24d20534efd4e6ac5528ea552b29ae6236e498879ff2c2f8642d"
 
-  head "https://github.com/albertyw/git-reviewers.git", :using => :git
+  head "https://github.com/albertyw/git-reviewers.git", using: :git
 
   depends_on "python"
 
