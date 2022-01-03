f546d3ff244e73e29eab14d4ff633b26f382c549
Albert Wang <git@albertyw.com>
2022-01-02 21:31:48 -0500

    Add req-update formula

---
+++ req-update.rb
@@ -0,0 +1,19 @@
+# typed: false
+# frozen_string_literal: true
+
+# ReqUpdate is a homebrew formula for the req-update cli tool
+class ReqUpdate < Formula
+  desc "Automatically update your project's dependencies"
+  homepage "https://github.com/albertyw/req-update"
+  url "https://github.com/albertyw/req-update/archive/v1.6.4.tar.gz"
+  sha256 "7b6a50e596b8a91e172d32c76c78ecb0591dd459c16c74c973a8b2d2b678e2c1"
+
+  head "https://github.com/albertyw/req-update.git", using: :git
+
+  depends_on "python"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"req_update/req_update.py"
+  end
+end
