e6898e00da4e8f79c75fb55a6a5246f95c9d143c
Albert Wang <git@albertyw.com>
2022-02-11 01:19:22 -0500

    Update req-update package to 2.0.0

--- req-update.rb
+++ req-update.rb
@@ -5,8 +5,8 @@
 class ReqUpdate < Formula
   desc "Automatically update your project's dependencies"
   homepage "https://github.com/albertyw/req-update"
-  url "https://github.com/albertyw/req-update/archive/v1.8.0.tar.gz"
-  sha256 "8d43b4ef3772fdef7c061a0c0ddb2524669e7ec66e572af12e60eeac986ae6b4"
+  url "https://github.com/albertyw/req-update/archive/v2.0.0.tar.gz"
+  sha256 "5e4719ff1a457df21ccf6a24c5abfaf9f56cc999100525f7d0ecbb12efb099c0"
   license "MIT"
 
   head "https://github.com/albertyw/req-update.git", using: :git
