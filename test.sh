d2347ea51c741cbda763ae1c73853777c467402e
Albert Wang <git@albertyw.com>
2023-05-29 17:05:57 -0700

    Also test req-update formula

--- test.sh
+++ test.sh
@@ -8,6 +8,8 @@ brew typecheck
 
 brew audit ./git-browse.rb --formula
 brew audit ./git-reviewers.rb --formula
+brew audit ./req-update.rb --formula
 
 brew install ./git-browse.rb --formula --build-from-source
 brew install ./git-reviewers.rb --formula --build-from-source
+brew install ./req-update.rb --formula --build-from-source
