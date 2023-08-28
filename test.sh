8d1f32cb16e376fdd78d4cadcbf5e285db1791c2
Albert Wang <git@albertyw.com>
2023-08-28 00:30:21 -0400

    Also test uninstall homebrew formulas

--- test.sh
+++ test.sh
@@ -11,5 +11,10 @@ brew typecheck
 # brew audit ./req-update.rb --formula
 
 brew install ./git-browse.rb --formula --build-from-source
+brew uninstall ./git-browse.rb --formula
+
 brew install ./git-reviewers.rb --formula --build-from-source
+brew uninstall ./git-reviewers.rb --formula
+
 brew install ./req-update.rb --formula --build-from-source
+brew uninstall ./req-update.rb --formula
