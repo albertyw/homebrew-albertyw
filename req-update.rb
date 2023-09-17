2a98422535ad672294cb3e24124ace38ceaad0ee
Albert Wang <git@albertyw.com>
2023-09-17 16:59:52 -0400

    Update req-update to v2.3.3

--- req-update.rb
+++ req-update.rb
@@ -5,8 +5,8 @@
 class ReqUpdate < Formula
   desc "Automatically update your project's dependencies"
   homepage "https://github.com/albertyw/req-update"
-  url "https://github.com/albertyw/req-update/archive/v2.3.2.tar.gz"
-  sha256 "d7999b13c181bb5c77c0ec43042b68a192a28cef4e7a8056bef14174d3d967bd"
+  url "https://github.com/albertyw/req-update/archive/v2.3.3.tar.gz"
+  sha256 "9f9d0f947cbc29ebcea2a1c1e5cd7fb55aec2f5069e322ea214b2f01ec993aa8"
   license "MIT"
 
   head "https://github.com/albertyw/req-update.git", using: :git
