4518723d54adf70c25df92eeb7f04c3675ca5316
Albert Wang <git@albertyw.com>
2024-06-23 01:05:01 -0700

    Update req-update to v2.7.1

--- req-update.rb
+++ req-update.rb
@@ -5,8 +5,8 @@
 class ReqUpdate < Formula
   desc "Automatically update your project's dependencies"
   homepage "https://github.com/albertyw/req-update"
-  url "https://github.com/albertyw/req-update/archive/v2.7.0.tar.gz"
-  sha256 "d510fd978abd1ba0c5fdd8f67b6e61cac2dbb37d4eebd68028ce6189b7073b7b"
+  url "https://github.com/albertyw/req-update/archive/v2.7.1.tar.gz"
+  sha256 "20d92362740b42c826e4cce7dcd7fbd77c0edf8fcf9b807bfbcd466cf36da71f"
   license "MIT"
 
   head "https://github.com/albertyw/req-update.git", using: :git
