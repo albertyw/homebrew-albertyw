a3558eac731f02912408db8a7d3d7056d8805b0a
Albert Wang <git@albertyw.com>
2025-08-03 15:27:51 -0700

    Fix sha256 for req-update

--- req-update.rb
+++ req-update.rb
@@ -6,7 +6,7 @@ class ReqUpdate < Formula
   desc "Automatically update your project's dependencies"
   homepage "https://github.com/albertyw/req-update"
   url "https://github.com/albertyw/req-update/archive/v2.7.5.tar.gz"
-  sha256 "698abdf16556979114b0507d01932bb58baf42bf02f0a9170444ddcee42cc983"
+  sha256 "98abdf16556979114b0507d01932bb58baf42bf02f0a9170444ddcee42cc9838"
   license "MIT"
 
   head "https://github.com/albertyw/req-update.git", using: :git
