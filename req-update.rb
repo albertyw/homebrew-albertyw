1e11e4e4b7139ab1e18f0a13b98cc648d80bc089
Albert Wang <git@albertyw.com>
2026-04-18 15:25:37 -0700

    Update req-update to v2.9.1

--- req-update.rb
+++ req-update.rb
@@ -5,8 +5,8 @@
 class ReqUpdate < Formula
   desc "Automatically update your project's dependencies"
   homepage "https://github.com/albertyw/req-update"
-  url "https://github.com/albertyw/req-update/archive/v2.9.0.tar.gz"
-  sha256 "e280fa83f0ed2759667d01b7db0265badc75cd2935b186fe291668582623d1ae"
+  url "https://github.com/albertyw/req-update/archive/v2.9.1.tar.gz"
+  sha256 "b336d81cace6694979c3c0a286a0173ea066cc2ce4d1ee9ef1084154fd62be97"
   license "MIT"
 
   head "https://github.com/albertyw/req-update.git", using: :git
