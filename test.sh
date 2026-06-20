7affdd2b0b2088bf80f056f1d97169c0359b2f53
Albert Wang <git@albertyw.com>
2026-06-19 21:43:36 -0700

    Re-enable brew audit and add brew test to CI script

--- test.sh
+++ test.sh
@@ -8,15 +8,9 @@ brew developer on
 
 brew typecheck
 
-# brew audit ./git-browse.rb --formula
-# brew audit ./git-reviewers.rb --formula
-# brew audit ./req-update.rb --formula
-
-brew install ./git-browse.rb --formula --build-from-source
-brew uninstall ./git-browse.rb --formula
-
-brew install ./git-reviewers.rb --formula --build-from-source
-brew uninstall ./git-reviewers.rb --formula
-
-brew install ./req-update.rb --formula --build-from-source
-brew uninstall ./req-update.rb --formula
+for formula in git-browse git-reviewers req-update; do
+  brew audit ./$formula.rb --formula --strict
+  brew install ./$formula.rb --formula --build-from-source
+  brew test ./$formula.rb --formula
+  brew uninstall ./$formula.rb --formula
+done
