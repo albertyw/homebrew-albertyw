979995c4a1f2f0550b6bb0376cf1a8ece98b9211
Albert Wang <git@albertyw.com>
2026-06-19 21:11:15 -0700

    Fix description for git-reviewers

--- git-reviewers.rb
+++ git-reviewers.rb
@@ -3,7 +3,7 @@
 
 # GitReviewers is a homebrew formula for the git-reviewers cli tool
 class GitReviewers < Formula
-  desc "Open git files, directories, commits, and branches in the reviewersr"
+  desc "Find code reviewers for a git commit"
   homepage "https://github.com/albertyw/git-reviewers"
   url "https://github.com/albertyw/git-reviewers/archive/v0.13.7.tar.gz"
   sha256 "509a91903b908b773b553b72ac513f7e37b8b2bcdac2a4a02626f9cd26bc5c81"
