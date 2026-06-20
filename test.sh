ad9379dc1f24b04e3b5d238d05c4c16b60e4af63
Albert Wang <git@albertyw.com>
2026-06-19 22:08:34 -0700

    Use safe.directory wildcard for CI git ownership check

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ brew developer on
 
 brew typecheck
 
-git config --global --add safe.directory "$(pwd)"
+git config --global --add safe.directory '*'
 brew tap albertyw/albertyw .
 
 for formula in git-browse git-reviewers req-update; do
